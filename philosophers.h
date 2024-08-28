c89277a799aad22ca9ac13b012903865fac84b1b
Lucas <lucas@MacBook-Pro-de-Lucas-2.local>
2024-08-28 14:28:16 +0200

    test threads & mutex

---
+++ philosophers.h
@@ -0,0 +1,5 @@
+#include <stdlib.h> // malloc, free
+#include <unistd.h> // usleep
+#include <stdbool.h> // boolean condition
+#include <pthread.h> // mutex, threading
+#include <sys/time.h> // get timne of the day
